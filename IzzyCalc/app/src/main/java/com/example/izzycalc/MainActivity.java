cc5fa50853d0ef561dfd182f677c248121db5675
IzzyGrant <nikaylez@gmail.com>
2022-07-04 23:46:14 -0500

    Ingresando proyecto Android al Repositorio.

---
+++ IzzyCalc/app/src/main/java/com/example/izzycalc/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.izzycalc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void Mundobtn(View view) {
+        TextView layout = findViewById(R.id.layout1);
+        layout.setText("Mundo");
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void Holabtn(View view) {
+        TextView layout = findViewById(R.id.layout1);
+        layout.setText("Hola");
+    }
+}
